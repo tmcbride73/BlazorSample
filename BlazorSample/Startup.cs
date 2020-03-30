f8326c27562f7a2c0b039dd5cb4089138fbf4490
tmcbride <tmcbride7373@gmail.com>
2020-03-30 17:06:54 -0400

    Changes for signalR on Azure.

--- BlazorSample/Startup.cs
+++ BlazorSample/Startup.cs
@@ -25,7 +25,6 @@ namespace BlazorSample
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
@@ -41,6 +40,7 @@ namespace BlazorSample
             });
             services.AddSingleton<UserService>();
             services.AddScoped<HttpClient>();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,7 +53,6 @@ namespace BlazorSample
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
